90b437798fe3311278d24bc93596789883e8e89f
leonardo-ssAlencar <leonardo.souza.santosalencar@gmail.com>
2022-05-09 13:33:35 -0300

    Terminada a questão MatrizC.c

--- Meus_codigos/src/lista2/MatrizC.c
+++ Meus_codigos/src/lista2/MatrizC.c
@@ -8,17 +8,26 @@ int main()
 {
  srand(time(NULL));
  int M[TAM][TAM];
- int pos[3];
+ int pos[3] = {0, 0,0};
  for(int i = 0; i < TAM; i++)
  {
   for(int j = 0; j < TAM*2; j++)
   {
-      M[i][j] = rand()%11;
-      
-      
+      M[i][j] = rand()%201;
+      printf("%d ", M[i][j]);
+
+      if(M[i][j] > pos[0])
+      {
+          pos[0] = M[i][j];
+          pos[1] = i + 1;
+          pos[2] = j + 1;
+      }
   }
   printf("\n");
  }
+
+ printf("O maior valor %d esta na posicao %d x %d", pos[0], pos[1], pos[2]);
+
 
  return 0;
 }
